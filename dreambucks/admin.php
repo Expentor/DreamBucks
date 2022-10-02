73f54598fa966322e258337818eb18df1c9ff8e8
morkmorquecho <105129572+morkmorquecho@users.noreply.github.com>
2022-10-02 15:30:12 -0500

    Delete dreambucks directory

--- dreambucks/admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-// este es el apartado del admi y el unico acceso para agregar usuarios nuevos
-echo "eres administrador"; 
-?>
-<a href="Register_U.php" class="">Craer cuenta para nuevo cliente</a>
-<a href="Logout.php" class="">Cerrar Sesion</a>
