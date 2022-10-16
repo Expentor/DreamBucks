b8c76ab064649e4d33504e4382226757fb6f7dbf
morkmorquecho <105129572+morkmorquecho@users.noreply.github.com>
2022-10-16 15:20:41 -0500

    alc le movi todo

--- dreambucks/index.php
+++
@@ -1,5 +0,0 @@
-<header>
-<!-- pagina de inicio, aqui escoges que tipo de usuario eres para hacer login -->
-<a href="Login_U.php" class="">Entrar como cliente</a>
-<a href="Login_A.php"  class="">Entrar como administrador </a>
-</header>
