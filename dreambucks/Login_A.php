68e139e7a1e4a5212fe961030ccb08021c1ad1cd
morkmorquecho <105129572+morkmorquecho@users.noreply.github.com>
2022-10-16 15:23:59 -0500

    el codigo que se presentara en el PI

---
+++ dreambucks/Login_A.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styleLoginA.css">
+    <link rel="stylesheet" href="./styles/styleHeaderUL.css">
+    <title>Administrador</title>
+</head>
+<body>
+    
+<!--SOLO DISEÑO-->
+
+
+    <header class="header">
+        <div class="container-head logo-nav-container">
+            <a href="#" class="logo">DREAMBUCKS</a>
+            <div class="menu-icon">Menú</div>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="#">Acerca de</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+       
+       <div class="form-container container">
+
+            <div class="photo-form">
+
+                
+            </div>
+
+            <form class="form" action="./Validate_A.php" method="POST">
+                    
+                <div class="form-text">
+                    <h2>Bienvenido de nuevo administrador</h2>
+                    <p>Inicie sesión con su cuenta</p>
+                </div>
+
+                <div class="input">
+                    <label for="user">Usuario</label>
+                    <input placeholder="Ingrese su usuario" type="text" id="id" name="id" pattern="[a-zA-Z0-9]+" required>
+                </div>
+                <div class="input">
+                    <label for="password">Contraseña</label>
+                    <input placeholder="Ingrese su contraseña" type="password" id="password" name="password" id="password" required>
+                </div>
+
+                <div class="forgot-password">
+                    <a href="#">¿Olvido su contraseña?</a>
+                </div>
+
+                <div class="input">
+                    <input type="submit" value="Iniciar" name="enviar">
+                </div>
+
+            </form>
+
+       </div> 
+
+    </main>
+
+
+    <?php
+
+        require "footer.html";
+
+    ?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="./scriptHUL.js"></script>
+
+</body>
+</html>
