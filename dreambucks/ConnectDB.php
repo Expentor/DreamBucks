0139368453a50cc95b4ae2f6f2b3d399034478da
morkmorquecho <105129572+morkmorquecho@users.noreply.github.com>
2022-10-21 19:55:43 -0500

    Delete dreambucks directory

--- dreambucks/ConnectDB.php
+++
@@ -1,5 +0,0 @@
-<?php
-// conectare a base de datos
-$connect = mysqli_connect("localhost", "root", "", "dreambucks");
-
-?>
--- dreambucks/Logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// cerrar sesion
-session_start(); 
-session_destroy();
-header('Location:index.php'); //duda
-  
-?>
