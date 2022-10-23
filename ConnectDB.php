7f012e50e19723c2c2bce3311c9d9116bda4bd57
Expentor <adairfernandez.aafs@gmail.com>
2022-10-23 15:46:29 -0500

    Change Database Link Method

--- ConnectDB.php
+++ ConnectDB.php
@@ -1,5 +1,16 @@
 <?php
-// conectare a base de datos
-$connect = mysqli_connect("localhost", "root", "Admin123?", "dreambucks");
 
-?>
+$host = "localhost";
+$database = "dreambucks";
+$user = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+  // foreach ($conn->query("SHOW DATABASES") as $row) {
+  //  print_r($row);
+  //}
+  //die();
+} catch (PDOException $e) {
+  die("PDO Connection Error: " . $e->getMessage());
+}
