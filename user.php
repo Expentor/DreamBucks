285d2e26310a1f7dd221cd0324dffcce1d3129d4
Neftali <neftalibarrera03@gmai.com>
2022-10-23 22:45:44 -0500

    User.php ya tiene estilo

--- user.php
+++ user.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./styles/styleHeaderUL.css">
-        <link rel="stylesheet" href="./styles/user.css">
+        <link rel="stylesheet" href="./styles/styleUser.css">
         <title>User</title>
 </head>
 <body>
