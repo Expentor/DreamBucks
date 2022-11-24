4773197a04430543cf7ff31a0028657ae57e1b73
HectorHYM <54413283+HectorHYM@users.noreply.github.com>
2022-11-23 22:38:14 -0600

    Avance del día 23/11/2022

---
+++ editUserConfirm.php
@@ -0,0 +1,122 @@
+<?php
+
+require "./ConnectDB.php";
+
+session_start();
+
+$clients = $conn->query("SELECT * FROM users");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styleHeader.css">
+    <link rel="stylesheet" href="../styles/styleCrudUser.css">
+    <link rel="stylesheet" href="./styles/styleAlert.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>Pagina de clientes</title>
+</head>
+<body>
+
+    <header class="header">
+        <div class="container-head logo-nav-container">
+            <a href="#" class="logo">DREAMBUCKS</a>
+            <div class="menu-icon">Menú</div>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="admin.php">Volver</a></li>
+                    <li><a href="CreateLoan.php">Crear prestamo</a></li>
+                    <li><a href="Register_U.php">Crear usuario</a></li>
+                    <li><a href="Credit_Recharge.php">Abonar saldo</a></li>
+                    <li><a href="Logout.php">Cerrar sesión</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <div class="crud-container">
+
+            <?php if ($clients->rowCount() == 0): ?>
+                <div class="col-md-4 mx-auto">
+                    <div class="card card-body text-center">
+                    <p>Todavia no tienes ningun cliente</p>
+                    <a href="CreateUser.php">Agrega a tu primer cliente</a>
+                </div>
+                </div>
+            <?php endif?>
+
+            <!-- Se iteran todos los clientes existentes en la base de datos -->
+            <table class="content-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Apellido Paterno</th>
+                            <th>Apellido Materno</th>
+                            <th>Email</th>
+                            <th>Debitado</th>
+                            <th>Direccion</th>
+                            <th>Telefono</th>
+                            <th>Saldo</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                <?php foreach ($clients as $client) { ?>
+                    <tbody>
+                        <tr>
+                            <td><p><?= $client["id_U"] ?></p></td>
+                            <td><p class="card-title text-capitalize"><?= htmlspecialchars($client["name_U"]) ?></p></td>
+                            <td><p class="card-title text-capitalize"><?= htmlspecialchars($client["lastname1_U"]) ?></p></td>
+                            <td><p class="card-title text-capitalize"><?= htmlspecialchars($client["lastname2_U"]) ?></p></td>
+                            <td><p><?= htmlspecialchars($client["email_U"]) ?></p></td>
+                            <td><p><?= htmlspecialchars($client["debited"]) ?></p></td>
+                            <td><p><?= htmlspecialchars($client["address_U"]) ?></p></td>
+                            <td><p><?= htmlspecialchars($client["phone"]) ?></p></td>
+                            <td><p><?= htmlspecialchars($client["balance"]) ?></p></td>
+                            <td><a href="editUser.php?id_U=<?= $client["id_U"] ?>" class="btn-edit">Editar</a></td>
+                            <td><a href="deleteUser.php?id_U=<?= $client["id_U"] ?>" class="btn-delete">Eliminar</a></td>
+                        </tr>
+                    </tbody>
+                <?php } ?>
+            </table>
+        </div>
+    </main>
+
+    <div class="confirmation show">
+        <span class="fas fa-exclamation-circle"></span>
+        <span class="msg">Usuario editado con éxito</span>
+        <span class="close-btn">
+             <span class="fas fa-times"></span>
+        <span>
+    </div>
+
+    <?php
+
+        require "footer.html"
+
+    ?>
+
+    <script>
+        $('.close-btn').click(function(){
+            $('.confirmation').addClass("hide");
+            $('.confirmation').removerClass("show");
+        }); 
+    </script>
+
+  <!-- bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" 
+  integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" 
+  crossorigin="anonymous"></script>
+  <!-- bootstrap -->
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="./scriptHUL.js"></script>
+
+</body>
+</html>
