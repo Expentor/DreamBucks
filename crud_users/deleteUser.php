ccd7e4d9405bcd973e7ece78d4415bd7febfa8e5
HectorHYM <54413283+HectorHYM@users.noreply.github.com>
2022-11-25 11:10:57 -0600

    25/11/2022

---
+++ crud_users/deleteUser.php
@@ -0,0 +1,21 @@
+<?php
+if (!isset($_SESSION["id_A"])) {
+  header("Location: index.php");
+  return;
+}
+require "../ConnectDB.php";
+
+$id = $_GET["id_U"];
+
+$statement = $conn->prepare("DELETE FROM users WHERE id_U = :id_U");
+$statement->execute([":id_U" => $id]);
+
+if ($statement->rowCount() == 0) {
+  http_response_code(404);
+  echo("HTTP 404 NOT FOUND");
+  return;
+}
+
+$conn->prepare("DELETE FROM users WHERE id_U = :id_U")->execute([":id_U" => $id]);
+
+header("Location: users_table.php");
